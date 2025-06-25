3a17836a8717e62005dcdc276259c60419855554
Shoes-163 <denysshevtsov7@gmail.com>
2025-06-25 12:02:05 +0300

    Added CORS settings

--- Pryshchepa_project/settings.py
+++ Pryshchepa_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'web_app',
     'rest_framework',
     'drf_spectacular',
@@ -55,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'Pryshchepa_project.urls'
@@ -110,6 +112,17 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+# CORS settings
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+]
+
+USE_X_FORWARDED_HOST = True
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Internationalization
